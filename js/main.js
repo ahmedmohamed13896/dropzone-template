696d74057c623ea2c778953cbdb35537102cf73e
ahmed mohamed <ahmedmohamed77765@hotmail.com>
2020-08-09 22:50:56 +0200

    fix Issues

--- js/main.js
+++ js/main.js
@@ -7,5 +7,16 @@ $(function () {
         parallelUploads: 2,
         dictDefaultMessage: "Drag files here or <br> click to select... ",
     });
+    var selected_link, selected_div;
+
+    $('.mynav .list li a.nav-link').on('click', function () {
+        selected_link = $(this).attr('data-link');
+        console.log(selected_link);
+        selected_div = $('.menu-selected ') + selected_link;
+        $('.mydropzone').hide();
+        $('.menu-selected .menu-div').hide();
+        $('.menu-selected ').fadeIn().children(selected_link).fadeIn();
+    });
+
 
 });
